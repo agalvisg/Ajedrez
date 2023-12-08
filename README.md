8e61a47eba8c50c4e7a40814534b12a990e03f32
agalvisg <agalvgut@myuax.com>
2023-12-08 20:13:25 +0100

    commit del readme y del archivo python

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Ajedrez
+
+
+En este intento de ajedrez, utilicé las listas.
+
+Dentro de el fichero están las especificaiones de en qué consiste el comando y qué hace cada función.
+
+Navegué entre las listas e "intercambié" sus elementos para poder relflejar el movimiento qeu se desee mediante coordenadas.
+
--- ajedrez.py
+++ ajedrez.py
@@ -1,40 +1,75 @@
 #AJEDREZ
  #función del tablero
 tablero_inicial = [
-                ["_","A","B","C","D","E","F","G","H","_"],
-                ["1","♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜","1"],
-                ["2","♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟","2"],
-                ["3","_", "_", "_", "_", "_", "_", "_", "_","3"],
-                ["4","_", "_", "_", "_", "_", "_", "_", "_","4"],
-                ["5","_", "_", "_", "_", "_", "_", "_", "_","5"],
-                ["6","_", "_", "_", "_", "_", "_", "_", "_","6"],
-                ["7","♙", "♙", "♙", "♙", "♙", "♙", "♙", "♙","7"],
-                ["8","♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖","8"],
-                ["_","A","B","C","D","E","F","G","H","_"]
+                ["♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜"],
+                ["♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟"],
+                ["_", "_", "_", "_", "_", "_", "_", "_"],
+                ["_", "_", "_", "_", "_", "_", "_", "_"],
+                ["_", "_", "_", "_", "_", "_", "_", "_"],
+                ["_", "_", "_", "_", "_", "_", "_", "_"],
+                ["♙", "♙", "♙", "♙", "♙", "♙", "♙", "♙"],
+                ["♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖"],
+                
                 ]
 
-
+#función de movimiento
 def movimiento():
-        pieza_seleccionada=tablero_inicial('fila'int(input),'columna'int(input))
-        nueva_posicion=tablero_inicial('fila'int(input),'columna'int(input))
-        tablero_nuevo=tablero_inicial.reverse(pieza_seleccionada,nueva_posicion)
-        print(tablero_nuevo)
-
-
+        
+        tablero=tablero_inicial
+        
+        #selección de pieza
+        
+        fila_pieza=int(input("Ingresa la fila de la pieza a mover: "))
+        columna_pieza=int(input("Ingresa la columna de la pieza a mover: "))
+        
+        #selección de nueva ubicación
+        
+        fila_nueva=int(input("Ingresa la nueva fila: "))
+        columna_nueva=int(input("Ingresa la nueva columna: "))
+        
+        #verificar que la elección está dentro del rango
+        if 0<=fila_pieza and 0<=columna_pieza and 0<=fila_nueva and 0<=columna_nueva:
+                pieza_seleccionada=tablero[fila_pieza][columna_pieza]
+                
+                tablero[fila_nueva][columna_nueva]=pieza_seleccionada
+                tablero[fila_pieza][columna_pieza]="-"
+                
+                return tablero
+        else:
+                print("Coordenadas fuera de rango")
+                return tablero_inicial
+        
+        
+#función de juego       
 def empezar():
 
-        eleccion=input("Hola!¿Quieres jugar ajedrez? (sí/no): ")
-        print(eleccion)
+        eleccion=input("Hola!¿Quieres jugar ajedrez? (sí/no): ").lower
+
+        #bucle de juego
         if eleccion=='sí':
-        # Imprimir el tablero inicial
+                print("En cuanto quieras acabar la partida escribe terminar.")
+                tablero_actual=tablero_inicial.copy()
+                print('Tablero Inicial: ')
                 for fila in tablero_inicial:
                         print("\t".join(fila))
-                print('¿Qué movimiento quieres hacer?(ingrésalo mediante coordenadas)')
-                movimiento()
-
+                while True:
+                        print('¿Qué movimiento quieres hacer?(Utiliza coordenadas)')
+                        tablero_actual=movimiento(tablero_actual)
+                        for fila in tablero_actual:
+                                print("\t".join(fila))
+                        terminar=input('¿Quieres terminar la partida?(sí/no)')
+                        if terminar=='sí':
+                                print('¡Gracias por jugar!')
+                                break
+                        
+                        
         elif eleccion=='no':
               print("¡Has salido!")
-              exit()    
+              exit() 
+        
+        else:
+                print('opción no válida') 
+                return eleccion  
 empezar()   
         
                
